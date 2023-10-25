5e0c7482b4922fa2cc0864532cbd6d40dd2ed242
deafult2010 <thomas.jaeger@blueyonder.co.uk>
2023-10-25 10:07:10 +0100

    first commit

---
+++ electron-app/src/renderer/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Clock from './Clock';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>React Clock</h1>
+      <Clock />
+    </div>
+  );
+};
+
+export default App;
